827b17c326d2994e34b82bfa11698b946be62ab3
Ajay Sharma <aj@ajsharma.com>
2014-01-27 03:04:05 -0800

    Fixes for tests

--- test/controllers/scores_controller_test.rb
+++ test/controllers/scores_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ScoresControllerTest < ActionController::TestCase
   setup do
-    @score = scores(:one)
+    @score = FactoryGirl.build(:score)
   end
 
   test "should get index" do
@@ -11,39 +11,11 @@ class ScoresControllerTest < ActionController::TestCase
     assert_not_nil assigns(:scores)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create score" do
     assert_difference('Score.count') do
-      post :create, score: { name: @score.name, score: @score.score }
+      post :create, score: { name: "Joe", score: 100 }
     end
 
     assert_redirected_to score_path(assigns(:score))
-  end
-
-  test "should show score" do
-    get :show, id: @score
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @score
-    assert_response :success
-  end
-
-  test "should update score" do
-    patch :update, id: @score, score: { name: @score.name, score: @score.score }
-    assert_redirected_to score_path(assigns(:score))
-  end
-
-  test "should destroy score" do
-    assert_difference('Score.count', -1) do
-      delete :destroy, id: @score
-    end
-
-    assert_redirected_to scores_path
   end
 end
